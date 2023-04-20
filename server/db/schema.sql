da96e7c02d980a81aa7c39f2dc3fd061f45cf15f
Jackie <jackiesixty@gmail.com>
2023-04-20 11:45:18 -0500

    Created logbreaker DB, users table, and games table

---
+++ server/db/schema.sql
@@ -0,0 +1,32 @@
+--create database if not exists `test`;
+
+drop database if exists logbreaker; 
+create database logbreaker;
+
+use logbreaker;
+
+-- create table users (
+    -- id int(11) unsigned not null auto_increment,
+    -- username varchar(255) not null,
+    -- password varchar(255) not null,
+    -- email varchar(255) not null,
+    -- created_at timestamp not null default current_timestamp,
+    -- updated_at timestamp not null default current_timestamp on update current_timestamp,
+    -- primary key (id)
+    -- );
+
+create table users )
+    id int not null primary key auto_increment,
+    username varchar(255) not null,
+    password varchar(255) not null,
+);
+
+create table games (
+    id int not null primary key auto_increment,
+    title varchar(255) not null,
+    --text [description] varchar(1000), 
+    summary varchar(1000),
+    id_user into not null,
+    foreign key (id_user) references users(id)
+); 
+
